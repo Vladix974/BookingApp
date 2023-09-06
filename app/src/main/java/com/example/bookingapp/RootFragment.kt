386af15cae6885a98f1fba0e0b099d2eb733a884
Vladix974 <lutsenkovladislav974@gmail.com>
2023-09-06 21:59:36 +0300

    add registration fragment

---
+++ app/src/main/java/com/example/bookingapp/RootFragment.kt
@@ -0,0 +1,13 @@
+package com.example.bookingapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RootFragment : Fragment(R.layout.fragment_root) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
